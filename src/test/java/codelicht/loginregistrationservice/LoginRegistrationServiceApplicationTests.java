69e549a5c877751d17d99b11fb01f06bdc3e49c1
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2024-08-05 11:10:01 -0500

    Inicialización del proyecto

---
+++ src/test/java/codelicht/loginregistrationservice/LoginRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package codelicht.loginregistrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistrationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
